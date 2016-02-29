f69513247e21279806d839268a5cb93e3a4318a9
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-02-29 13:43:29 -0600

    vs985: initial carbon port from cm

--- carbon_vs985.mk
+++ carbon_vs985.mk
@@ -1,12 +1,10 @@
 $(call inherit-product, device/lge/vs985/full_vs985.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/cdma.mk)
+$(call inherit-product, vendor/carbon/config/common.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-PRODUCT_NAME := cm_vs985
+PRODUCT_NAME := carbon_vs985
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
